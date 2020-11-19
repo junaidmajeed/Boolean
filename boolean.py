2d16d8085781a84b48ac7817fe21d3fee78c2041
junaidmajeed <35539538+junaidmajeed@users.noreply.github.com>
2020-11-19 16:52:24 +0500

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,19 @@
+"""
+Examples to show how boolean works in python
+"""
+
+a =True
+b = False
+
+print(a)
+print(b)
+print(10*'*')
+print(bool(0))
+print(bool(1))
+print(bool(2))
+
+c = ""
+print(bool(c))
+
+d = "Some Value"
+print(bool(d))
